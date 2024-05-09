a6c34a9319edab930404ac5a144fc69504cddc76
Gianmarcos97 <gianmarcoep@gmail.com>
2024-05-09 18:41:34 -0500

    subida dias reto

---
+++ Day08-circular/código.R
@@ -0,0 +1,9 @@
+
+datos <- read.csv("C:/Users/Acer/Desktop/GianmarcosEspinoza_trabajo_práctico/Day08-circular/paises-con-mas-aportes.csv", header = TRUE, stringsAsFactors = FALSE)
+datos <- head(datos, 6)
+
+# Crear un gráfico circular con etiquetas de valores
+pie(datos$Documents, labels = paste(datos$Country, "\n", datos$Documents), main = "Top 6 países con más contribuciones a la IA en 2020", col = rainbow(length(datos$Documents)))
+
+# Agregar leyenda
+legend("topright", datos$Country, fill = rainbow(length(datos$Documents)), cex = 0.8)
